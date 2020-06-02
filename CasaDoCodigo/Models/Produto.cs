8727aa59ed26e86e25f211cd86d10516efeb992a
douglas souto de souza <douglas_bufao@yahoo.com.br>
2020-06-02 17:05:18 -0300

    Criação das entidades do projeto

---
+++ CasaDoCodigo/Models/Produto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CasaDoCodigo.Models
+{
+    public class Produto : BaseModel
+    {
+        [Required]
+        public string Codigo { get; private set; }
+        [Required]
+        public string Nome { get; private set; }
+        [Required]
+        public decimal Preco { get; private set; }
+
+        public Produto()
+        {
+
+        }
+
+        public Produto(string codigo, string nome, decimal preco)
+        {
+            this.Codigo = codigo;
+            this.Nome = nome;
+            this.Preco = preco;
+        }
+    }
+}
